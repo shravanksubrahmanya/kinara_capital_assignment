bf6f975b40c7f75ab29c1fa1dceabfd98189d0ea
shravanksubrahmanya <shravanksubrahmanya@gmail.com>
2023-05-27 06:52:16 +0530

    Completed basic functionalities for the web application to run

--- kinara_capital_assignment/settings.py
+++ kinara_capital_assignment/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'student_grid',
-    'bootstrap4'
+    'django_select2',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
---
+++ student_grid/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from . import views
+# from django.views.i18n import JavaScriptCatalog
+
+app_name='student_grid'
+
+urlpatterns = [
+]
